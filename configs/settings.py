a5aaa11e1195707b574a6132c34c21ab0102068e
朗月 <zhaoyu.qin@foxmail.com>
2024-10-01 16:55:04 +0800

    feat: prototype test

--- configs/settings.py
+++ configs/settings.py
@@ -29,7 +29,7 @@ file_handler = logging.handlers.RotatingFileHandler(
     log_file, maxBytes=1024 * 1024 * 10, backupCount=10
 )
 file_handler.setFormatter(formatter)
-file_handler.setLevel(logging.INFO)
+file_handler.setLevel(logging.DEBUG)
 logger.addHandler(file_handler)
 
 console_handler = logging.StreamHandler()
