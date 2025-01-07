8b94cf5f3772a41571a3c2193b320b69e1d64788
朗月 <zhaoyu.qin@foxmail.com>
2025-01-07 14:51:30 +0800

    feat: 预定义fdm算子

---
+++ core/solvers/fdm/operators/__init__.py
@@ -0,0 +1,17 @@
+from core.solvers.fdm.operators.curls import *
+from core.solvers.fdm.operators.divs import *
+from core.solvers.fdm.operators.laplacians import *
+from core.solvers.fdm.operators.grads import *
+from core.solvers.fdm.operators.d2dt2s import *
+from core.solvers.fdm.operators.ddts import *
+
+
+# register all the fdm operators
+fdm_operators = {}
+for name, obj in list(locals().items()):
+    if isinstance(obj, type) and issubclass(obj, IOperator):
+        if name == "IOperator":
+            continue
+        if name in fdm_operators:
+            raise ValueError(f"Duplicated fdm operator: {name}.")
+        fdm_operators[name] = obj
