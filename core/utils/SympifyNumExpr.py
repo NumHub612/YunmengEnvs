f20fc267c9e3f91840a6c33750ce04ba47d321a3
朗月 <zhaoyu.qin@foxmail.com>
2025-01-15 09:35:34 +0800

    Fix ci (#40)
    * ci: Update codecov
    
    * ci: 测试

--- core/utils/SympifyNumExpr.py
+++ core/utils/SympifyNumExpr.py
@@ -23,25 +23,3 @@ def lambdify_numexpr(expr: str, symbols: list) -> Callable:
     return lambdify(
         symbols, sp.sympify(expr, locals=dict(zip(symbols, sp.symbols(symbols))))
     )
-
-
-if __name__ == "__main__":
-    expr = "x**2 + y**2"
-    symbols = ["x", "y"]
-    f = lambdify_numexpr(expr, symbols)
-    print(f(2, 3))  # Output: 13.0
-
-    """\begin{eqnarray}
-    u &=& -\frac{2 \nu}{\phi} \frac{\partial \phi}{\partial x} + 4 \
-
-    \phi &=& \exp \bigg(\frac{-(x-4t)^2}{4 \nu (t+1)} \bigg) + \exp \bigg(\frac{-(x-4t -2 \pi)^2}{4 \nu(t+1)} \bigg)
-    \end{eqnarray}"""
-
-    phi = (
-        "exp(-(-4*t + x - 2*pi)**2/(4*nu*(t + 1))) + exp(-(-4*t + x)**2/(4*nu*(t + 1)))"
-    )
-    phiprime = "-(-8*t + 2*x)*exp(-(-4*t + x)**2/(4*nu*(t + 1)))/(4*nu*(t + 1)) - (-8*t + 2*x - 4*pi)*exp(-(-4*t + x - 2*pi)**2/(4*nu*(t + 1)))/(4*nu*(t + 1))"
-    expr = f"-2 * nu * (({phiprime}) / ({phi})) + 4"
-    symbols = ["t", "x", "nu"]
-    f = lambdify_numexpr(expr, symbols)
-    print(f(1, 4, 3))  # Output: 3.49170664206445
