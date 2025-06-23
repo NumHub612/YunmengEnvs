457c480330e202845e2af20d71040556689a6722
朗月 <zhaoyu.qin@foxmail.com>
2025-06-23 10:58:48 +0800

    feat: init

---
+++ core/solvers/fvm/Convection2D.py
@@ -0,0 +1,6 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (C) 2025, The YunmengEnvs Contributors. Welcome aboard YunmengEnvs!
+
+Solutions for the 2D convection equation using finite volume method.
+"""
