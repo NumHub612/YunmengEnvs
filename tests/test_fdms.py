7833527f768e896191e82dcf360ee504f2271fd7
朗月 <zhaoyu.qin@foxmail.com>
2026-05-08 13:27:08 +0800

    test: 简化fdm测试

--- tests/test_fdms.py
+++ tests/test_fdms.py
@@ -25,54 +25,54 @@ from yunmeng.render.plotter.FieldPlotters import plot_field
 
 
 @pytest.fixture
-def grid_101x101() -> Grid2D:
+def grid_41x41() -> Grid2D:
     """Create a flat 2D grid for testing."""
     ll, ur = Coordinate(0, 0), Coordinate(2.0, 2.0)
-    grid_101x101 = Grid2D.by_uniform(ll, ur, 101, 101)
+    grid_41x41 = Grid2D.by_uniform(ll, ur, 41, 41)
 
     # Add elevation modifier
     modifier = ElevationModifier()
-    beds = np.ones(grid_101x101.node_count)
-    grid_101x101.modify(modifier, elevations=beds, etype=ElementType.NODE, max_dz=2.0)
-    return grid_101x101
+    beds = np.ones(grid_41x41.node_count)
+    grid_41x41.modify(modifier, elevations=beds, etype=ElementType.NODE, max_dz=2.0)
+    return grid_41x41
 
 
 @pytest.fixture
-def H0(grid_101x101: Grid2D) -> Field:
+def H0(grid_41x41: Grid2D) -> Field:
     """Initial condition for the water depth field."""
     init_val = 1.0
     H0 = Field.from_size(
-        grid_101x101.node_count, VariableType.SCALAR, ElementType.NODE, init_val
+        grid_41x41.node_count, VariableType.SCALAR, ElementType.NODE, init_val
     )
 
     # Gaussian perturbation
-    Lx = grid_101x101.lx
-    Ly = grid_101x101.ly
-    Nx = grid_101x101.nx
-    Ny = grid_101x101.ny
+    Lx = grid_41x41.lx
+    Ly = grid_41x41.ly
+    Nx = grid_41x41.nx
+    Ny = grid_41x41.ny
     xc = Lx / 2
     yc = Ly / 2
     for i in range(Nx):
         for j in range(Ny):
-            idx = grid_101x101.match_node(i, j)
-            X = grid_101x101.nodes[idx].coordinate.x
-            Y = grid_101x101.nodes[idx].coordinate.y
+            idx = grid_41x41.match_node(i, j)
+            X = grid_41x41.nodes[idx].coordinate.x
+            Y = grid_41x41.nodes[idx].coordinate.y
             dz = 0.5 * np.exp(-((X - xc) ** 2 + (Y - yc) ** 2) / (2 * 0.5**2))
             H0[idx] += dz
     return H0
 
 
 @pytest.fixture
-def U0(grid_101x101: Grid2D) -> Field:
+def U0(grid_41x41: Grid2D) -> Field:
     """Initial condition for the velocity field."""
     init_val = Variable.vector(1.0, 1.0, 0.0)
     U0 = Field.from_size(
-        grid_101x101.node_count, VariableType.VECTOR, ElementType.NODE, init_val
+        grid_41x41.node_count, VariableType.VECTOR, ElementType.NODE, init_val
     )
 
-    nx, ny = grid_101x101.nx, grid_101x101.ny
-    dx = grid_101x101.lx / (nx - 1)
-    dy = grid_101x101.ly / (ny - 1)
+    nx, ny = grid_41x41.nx, grid_41x41.ny
+    dx = grid_41x41.lx / (nx - 1)
+    dy = grid_41x41.ly / (ny - 1)
 
     y_start = int(0.5 / dy)
     y_end = int(1 / dy + 1)
@@ -81,7 +81,7 @@ def U0(grid_101x101: Grid2D) -> Field:
 
     for i in range(nx):
         for j in range(ny):
-            idx = grid_101x101.match_node(i, j)
+            idx = grid_41x41.match_node(i, j)
             if y_start <= j <= y_end and x_start <= i <= x_end:
                 U0[idx] = Variable.vector(2.0, 2.0, 0.0)
 
@@ -96,17 +96,17 @@ def U0(grid_101x101: Grid2D) -> Field:
 class TestBurgers2D:
     """Test suite for Burgers2D solver."""
 
-    def test_2d_grid(self, grid_101x101: Grid2D, H0: Field, U0: Field):
+    def test_2d_grid(self, grid_41x41: Grid2D, H0: Field, U0: Field):
         """Test burgers2d on grid2d."""
         # visualize the grid and initial conditions
         plot_mesh(
-            grid_101x101,
-            title="grid_101x101",
+            grid_41x41,
+            title="grid_41x41",
             save_dir="tests/results/",
             show_edges=True,
         )
         plot_field(
-            U0, grid_101x101, title="U0", save_dir="tests/results/", show_edges=True
+            U0, grid_41x41, title="U0", save_dir="tests/results/", show_edges=True
         )
 
         # initial condition
@@ -114,7 +114,7 @@ class TestBurgers2D:
 
         # boundary condition
         value_bc = ValueBoundary("bc", Var([1.0, 1.0, 0.0]))
-        bc_nodes = grid_101x101.get_topo_assistant().boundary_nodes
+        bc_nodes = grid_41x41.get_topo_assistant().boundary_nodes
 
         # callbacks
         cb = ImageRender("render", "tests/results/", frequency=0.05)
@@ -132,19 +132,19 @@ class TestBurgers2D:
 
         operators = {
             "grad": Grad01(),
-            "lap": Lap01(diffusivity=0.005),
+            "lap": Lap01(diffusivity=0.01),
             "src": Src01(tau=1.0, source_func=source_func),
         }
 
         # solver
-        solver = BurgersExplicitSolver("solver", grid_101x101, operators)
+        solver = BurgersExplicitSolver("solver", grid_41x41, operators)
         solver.add_ic("u", u_init)
         solver.add_bc("u", value_bc, bc_nodes, ElementType.NODE)
         solver.add_callback(cb)
 
         # initialize
         total_time = 1.0
-        solver.initialize(total_time, time_step=0.008, cfl=0.5)
+        solver.initialize(total_time, time_step=0.002, cfl=0.5)
 
         # run the simulation
         t, dt = 0.0, total_time / 10
@@ -162,7 +162,88 @@ class TestBurgers2D:
         u_end = solver.get_solution("u")
         plot_field(
             u_end,
-            grid_101x101,
+            grid_41x41,
+            title="u_end",
+            save_dir="tests/results/",
+            show_edges=True,
+        )
+
+
+# ============================================
+# region Navier-Stokes Tests
+# ============================================
+
+
+class TestNavierStokes2D:
+    """Test suite for NavierStokes2D solver."""
+
+    def test_2d_grid(self, grid_41x41: Grid2D, H0: Field, U0: Field):
+        """Test burgers2d on grid2d."""
+        # visualize the grid and initial conditions
+        plot_mesh(
+            grid_41x41,
+            title="grid_41x41",
+            save_dir="tests/results/",
+            show_edges=True,
+        )
+        plot_field(
+            U0, grid_41x41, title="U0", save_dir="tests/results/", show_edges=True
+        )
+
+        # initial condition
+        u_init = HotstartInitialization("U0", U0)
+
+        # boundary condition
+        value_bc = ValueBoundary("bc", Var([1.0, 1.0, 0.0]))
+        bc_nodes = grid_41x41.get_topo_assistant().boundary_nodes
+
+        # callbacks
+        cb = ImageRender("render", "tests/results/", frequency=0.05)
+
+        # operators
+        def source_func(loc: Coordinate, u: Variable) -> float:
+            # Gaussian forcing
+            x, y, z = loc.x, loc.y, loc.z
+            if 0.5 <= x <= 1.0 and 0.5 <= y <= 1.0:
+                v = np.exp(-((x - 0.75) ** 2 + (y - 0.75) ** 2))
+            else:
+                v = 0.0
+            forcing = Variable.vector(v, v, 0.0)
+            return forcing
+
+        operators = {
+            "grad": Grad01(),
+            "lap": Lap01(diffusivity=0.01),
+            "src": Src01(tau=1.0, source_func=source_func),
+        }
+
+        # solver
+        solver = BurgersExplicitSolver("solver", grid_41x41, operators)
+        solver.add_ic("u", u_init)
+        solver.add_bc("u", value_bc, bc_nodes, ElementType.NODE)
+        solver.add_callback(cb)
+
+        # initialize
+        total_time = 1.0
+        solver.initialize(total_time, time_step=0.002, cfl=0.5)
+
+        # run the simulation
+        # t, dt = 0.0, total_time / 10
+        # while not solver.status.finished:
+        #     status = solver.inference()
+        #     if status.current_time >= t or status.finished:
+        #         t += dt
+        #         print(
+        #             f"Time: {status.current_time:.4f} / {status.end_time:.4f}, "
+        #             f"Time step: {status.time_step:.4f}, "
+        #             f"Step time: {status.step_time:.4f}"
+        #         )
+
+        # visualize results
+        u_end = solver.get_solution("u")
+        plot_field(
+            u_end,
+            grid_41x41,
             title="u_end",
             save_dir="tests/results/",
             show_edges=True,
